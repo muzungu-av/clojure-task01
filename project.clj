0a19ad7f3d4e2993d5d9e02e06fa128f89688f56
Arykin Valera <muzungu.av@gmail.com>
2020-12-02 16:12:11 +0300

    begin

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject csvdb "0.1.0-SNAPSHOT"
+  :description "The application is a simple file database"
+  :url ""
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [clojure-csv "2.0.1"]]
+  :repl-options {:init-ns csvdb.core}
+  :resource-paths ["resources"]
+  :plugins [[lein-cloverage "1.0.7-SNAPSHOT"]])
