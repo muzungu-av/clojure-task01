79500be8e4b28251125e60766c3088550e23afae
Arykin Valera <muzungu.av@gmail.com>
2020-12-02 16:14:31 +0300

    url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject csvdb "0.1.0-SNAPSHOT"
   :description "The application is a simple file database"
-  :url ""
+  :url "https://github.com/muzungu-av/clojure-task01-csvdb"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
