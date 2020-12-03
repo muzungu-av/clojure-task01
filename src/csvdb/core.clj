12ebefe5ee13f7712d181f8985e4840a9478fccc
Arykin Valera <muzungu.av@gmail.com>
2020-12-03 13:04:17 +0300

    if-not nil

--- src/csvdb/core.clj
+++ src/csvdb/core.clj
@@ -65,20 +65,26 @@
 ;;
 ;; Hint: if-not, filter
 (defn where* [data condition-func]
-  (filter condition-func data))   ;if-not  why ?
+  (if-not  (nil? condition-func)
+     (filter condition-func data)
+     data))
 
 ;; (limit* student 1)
 ;; => ({:surname "Ivanov", :year 1998, :id 1})
 ;;
 ;; Hint: if-not, take
 (defn limit* [data lim]
-  (take lim data))
+  (if-not  (nil? lim)
+    (take lim data)
+    data))
 
 ;; (order-by* student :year)
 ;; => ({:surname "Sidorov", :year 1996, :id 3} {:surname "Petrov", :year 1997, :id 2} {:surname "Ivanov", :year 1998, :id 1})
 ;; Hint: if-not, sort-by
 (defn order-by* [data column]
-  :ImplementMe!)
+  (if-not  (nil? column)
+    (sort-by column data)
+    data))
 
 ;; (join* (join* student-subject :student_id student :id) :subject_id subject :id)
 ;; => [{:subject "Math", :subject_id 1, :surname "Ivanov", :year 1998, :student_id 1, :id 1}
